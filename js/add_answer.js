eb724cbbca9e12124bfb0216920ea4cc8061268e
Bismus Alexey <shmelmega@gmail.com>
2023-05-06 22:41:52 +0300

    Added remaining form fields, needs more work

--- js/add_answer.js
+++ js/add_answer.js
@@ -1,58 +1,103 @@
-function addAnswer(sectionId) {
-  // Получаем значение поля ввода
-  const input = document.querySelector(`#${sectionId} .answer-input`);
-  const answer = input.value.trim();
 
-  // Если поле ввода пустое, ничего не делаем
-  if (answer === "") {
-    return;
+  function addAnswer(sectionId) {
+    const input = document.querySelector(`#${sectionId} .answer-input`);
+    const answer = input.value.trim();
+
+    if (answer === "") {
+      return;
+    }
+
+    const answerList = document.querySelector(`#${sectionId} .answer-list`);
+
+    const duplicateItem = Array.from(answerList.querySelectorAll('li span'))
+      .find(item => item.textContent === answer);
+
+    if (duplicateItem !== undefined) {
+      input.value = "";
+      return;
+    }
+
+    let editingItem = answerList.querySelector(".editing");
+    if (editingItem !== null) {
+      editingItem.firstChild.textContent = answer;
+      editingItem.classList.remove("editing");
+      input.value = "";
+      updateCheckbox();
+      return;
+    }
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.textContent = answer;
+    li.appendChild(span);
+
+    li.onclick = function () {
+      answerList.querySelectorAll("li").forEach(function (item) {
+        item.classList.remove("editing");
+      });
+      li.classList.add("editing");
+      input.value = span.textContent;
+    };
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Удалить";
+    deleteButton.onclick = function () {
+      li.remove();
+      updateCheckbox();
+    };
+    li.appendChild(deleteButton);
+
+    answerList.appendChild(li);
+
+    input.value = "";
+    updateCheckbox();
   }
 
-  // Получаем список ответов
-  const answerList = document.querySelector(`#${sectionId} .answer-list`);
+  function updateCheckbox() {
+  const answerList = document.querySelector("#list_is .answer-list");
+  const isAnswerList = document.querySelector("#is_answer_list");
+  isAnswerList.innerHTML = ""; // очищаем список ответов второй секции
 
-  // Если есть редактируемый элемент, то меняем его текст на новое значение
-  let editingItem = answerList.querySelector(".editing");
-  if (editingItem !== null) {
-    editingItem.firstChild.textContent = answer;
-    editingItem.classList.remove("editing");
-    input.value = ""; // Очищаем поле ввода
-    return;
-  }
-
-  // Создаем элемент списка
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.textContent = answer;
-  li.appendChild(span);
-
-  // Добавляем возможность редактировать элемент списка
-  li.onclick = function() {
-    // Снимаем выделение с других элементов
-    answerList.querySelectorAll("li").forEach(function(item) {
-      item.classList.remove("editing");
+  answerList.querySelectorAll("li").forEach(function (item) {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.value = item.firstChild.textContent;
+    checkbox.checked = Array.from(
+      isAnswerList.querySelectorAll("li")
+    ).some(function (li) {
+      return li.textContent === checkbox.value;
     });
+    checkbox.onchange = function () {
+      const li = Array.from(isAnswerList.querySelectorAll("li")).find(
+        function (li) {
+          return li.textContent === checkbox.value;
+        }
+      );
 
-    // Выделяем редактируемый элемент
-    li.classList.add("editing");
-    input.value = span.textContent;
-  };
+      if (checkbox.checked) {
+        if (!li) {
+          const li = document.createElement("li");
+          li.textContent = checkbox.value;
+          isAnswerList.appendChild(li);
+        }
+      } else {
+        if (li) {
+          li.remove();
+        }
+      }
+    };
 
-  // Создаем кнопку "Удалить"
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Удалить";
-  deleteButton.onclick = function() {
-    li.remove();
-  };
-  li.appendChild(deleteButton);
+    const label = document.createElement("label");
+    label.textContent = item.firstChild.textContent;
+    label.insertBefore(checkbox, label.firstChild);
 
-  // Добавляем элемент в список
-  answerList.appendChild(li);
-
-  // Очищаем поле ввода
-  input.value = "";
+    const li = document.createElement("li");
+    li.appendChild(label);
+    isAnswerList.appendChild(li);
+  });
 }
 
+  updateCheckbox();
 
 function showField(radioId, fieldId) {
   const radio = document.getElementById(radioId);
