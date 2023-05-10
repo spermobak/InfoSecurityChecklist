97436d523acbc2f56e5470bd291e2eb1c8bebb2e
Bismus Alexey <shmelmega@gmail.com>
2023-05-10 14:41:37 +0300

    add sticky hover menu after scrolling

--- checklist_page/js/scroll.js
+++ checklist_page/js/scroll.js
@@ -1,10 +1,35 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("h1").style.top = "0";
-  } else {
-    document.getElementById("h1").style.top = "-50px";
+let lastScrollTop = 0;
+
+const header = document.querySelector('.header');
+const menu = document.querySelector('.hover-underline-menu');
+const formLeft = document.querySelector('.form-left');
+const formRight = document.querySelector('.form-right');
+
+
+const headerHeight = header.offsetHeight;
+
+
+window.addEventListener('scroll', function() {
+  const scrollTop = window.pageYOffset;
+
+ 
+  if (scrollTop <= headerHeight) {
+    menu.style.position = 'static';
+    formLeft.style.marginTop = '0';
+  } 
+ 
+  else {
+    menu.style.position = 'fixed';
+    menu.style.top = '0';
+    menu.style.left = '0';
+    menu.style.right = '0';
+    formLeft.style.marginTop = `${menu.offsetHeight}px`;
   }
-  prevScrollpos = currentScrollPos;
-}
+
+  if (scrollTop > lastScrollTop) {
+    header.style.top = `-${headerHeight}px`;
+  } else {
+    header.style.top = '0';
+  }
+  lastScrollTop = scrollTop;
+});
