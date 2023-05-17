7958e1eb77dc2a3d097c5a43178bb303cb2bd06d
Alexey Sokolov <karkarych08@gmail.com>
2023-05-17 14:12:56 +0300

    Complete JSON, almost

--- checklist_page/js/Json.js
+++ checklist_page/js/Json.js
@@ -19,63 +19,7 @@ function submitForm() {
     //     return;
     // } else {
         var today = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
-        var rft = true;
-        if (formData.get("switch") == "yes"){
-            rft=true;
-        }else{
-            rft=false;
-        }
-        var pdbp = true;
-        if (formData.get("switch_bp") == "yes"){
-            pdbp = true;
-        }else{
-            pdbp = false;
-        }
-        var b_i = false;
-        if (formData.get("biometric") == "on"){
-            b_i = true;
-        }
-        var f_i = false;
-        if (formData.get("free_info") == "on"){
-            f_i = true;
-        }
-        var sp_i=false;
-        if (formData.get("special") == "on"){
-            sp_i = true;
-        }
-        var e_p = false;
-        if (formData.get("company_employees") == "on"){
-            e_p = true;
-        }
-        var c_p = false;
-        if (formData.get("company_clients") == "on"){
-            c_p = true;
-        }
-        var o_p = false;
-        if (formData.get("special") == "on"){
-            o_p = true;
-        }
-        var skzi = false;
-        if (formData.get("switch_c") == "yes"){
-            skzi = true;
-        }
-        var t_m = false;
-        if (formData.get("switch_rm") == "yes"){
-            t_m = true;
-        }
-        var p_p = false;
-        if (formData.get("switch_pp") == "yes"){
-            p_p = true;
-        }
-        var o_d = false;
-        if (formData.get("switch_od") == "yes"){
-            o_d = true;
-        }
-        var s_l = false;
-        if (formData.get("switch_sl") == "yes"){
-            s_l = true;
-        }
-        const jsonObject = {
+        var jsonObject = {
             addInfo: {
                 description: formData.get("name_org"),
                 email: formData.get("email"),
@@ -86,30 +30,30 @@ function submitForm() {
             criteria: [
                 {
                     responsibleIs: formData.get("numb_of_responsible_for_is"),
-                    responsibleForProtect:rft,
+                    responsibleForProtect: booleanification("switch"),
                     numOfTerritories: 3,
-                    personalDataBusinessProcess: pdbp,
+                    personalDataBusinessProcess: booleanification("switch_bp"),
                     numOfEmployees: formData.get("count_of_employee"),
                     numOfClients: formData.get("count_of_client"),
-                    publiclyInfo: f_i,
-                    biometricInfo: b_i,
-                    specialInfo: sp_i,
-                    employeeProcessing: e_p,
-                    clientsProcessing: c_p,
-                    otherProcessing: o_p,
+                    publiclyInfo: booleanification("free_info"),
+                    biometricInfo: booleanification("biometric"),
+                    specialInfo: booleanification("special"),
+                    employeeProcessing: booleanification("company_employees"),
+                    clientsProcessing: booleanification("company_clients"),
+                    otherProcessing: booleanification("special"),
                     numOfServers: formData.get("numb_of_servers"),
                     arm: formData.get("numb_of_aws"),
-                    skzi: skzi,
-                    threatModel: t_m,
-                    personalDataProtectPolicy: p_p,
-                    otherDocumentsOfPdProtect: o_d,
-                    securityLevel: s_l,
+                    skzi: booleanification("switch_c"),
+                    threatModel: booleanification("switch_rm"),
+                    personalDataProtectPolicy: booleanification("switch_pp"),
+                    otherDocumentsOfPdProtect: booleanification("switch_od"),
+                    securityLevel: booleanification("switch_sl"),
                     degreeOfDocumentation: formData.get("range_input")
                 }
             ],
             reports: [
                 {
-                    reportsPath: "Еще более новый путь до акта"
+                    reportsPath: null
                 }
             ],
             meansOfProtections: [
@@ -123,31 +67,67 @@ function submitForm() {
                     securityMonitoringTool: formData.get("securityMonitoringTool")
                 }
             ],
-            informationSystems: [
-                {
-                    system: "Kali Linux",
-                    foreign: true
-                },
-                {
-                    system: "Windows",
-                    foreign: true
-                }
-            ],
+            informationSystems: [],
             addresses: [
                 {
-                    address: "Санкт-Петербург, Xrfkjdcrfz"
+                    address: dearreyfication("address")
                 }
             ],
-            businessProcesses: [
-                {
-                    businessProcess: "Сбор данных454545"
-                },
-                {
-                    businessProcess: "Сбор данных657486/87"
-                }
-            ]
+            businessProcesses: []
         };
+        const bp_list = dearreyficationlite("buisness_processes");
+        for (let index = 0; index < bp_list.length; index++) {
+            const buisness_processes = bp_list[index];
+            jsonObject.businessProcesses.push({businessProcess : buisness_processes});   
+        }
+
+        const is_list = dearreyficationlite("list_is");
+        for (let index = 0; index < is_list.length; index++) {
+            const system = is_list[index];
+            const answer = document.querySelector("#is_answer_list input[name=" + system + "]");
+            var foreign = false;
+            console.log(answer.value);
+            if (answer.value == "on"){
+                foreign = true;
+            }
+            jsonObject.informationSystems.push({system : system, foreign : foreign});   
+        }
 
         console.log(JSON.stringify(jsonObject));
     // }
+}
+
+function booleanification(name) {
+    const form = document.querySelector("form");
+    const formData = new FormData(form);
+    if ((formData.get(name) == "yes") || (formData.get(name) == "on")){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function dearreyfication(name) {
+    const answerList = document.querySelectorAll(`#${name} .answer-list li span`);
+    var str = "";
+    if (answerList.length != 0){
+    for (let index = 0; index < (answerList.length - 1); index++) {
+        const answer = answerList[index];
+            str = str + answer.textContent + ", ";
+    }
+    str = str + answerList[answerList.length - 1].textContent;
+}
+    return str
+}
+
+function dearreyficationlite(name) {
+    const answerList = document.querySelectorAll(`#${name} .answer-list li span`);
+    var list = [];
+    if (answerList.length != 0){
+    for (let index = 0; index < (answerList.length ); index++) {
+        const answer = answerList[index];
+            list.push(answer.textContent);
+    }
+}
+    return list
 }
